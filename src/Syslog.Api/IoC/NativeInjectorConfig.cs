48eb82d27762751e2749a1c4ecc20866e0373d30
Marcelo Kortkamp <marcelusmedius@gmail.com>
2023-09-19 08:23:01 -0300

    feat: split IoC initializer

---
+++ src/Syslog.Api/IoC/NativeInjectorConfig.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using Syslog.Api.Providers.CodeGenerator;
+
+namespace Syslog.Api.IoC
+{
+    public static class NativeInjectorConfig
+    {
+        public static void RegisterService(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+            services.AddScoped<ICodeGenerator, CodeGenerator>();
+        }
+    }
+}
--- src/Syslog.Api/Program.cs
+++ src/Syslog.Api/Program.cs
@@ -1,6 +1,14 @@
+using Syslog.Api.IoC;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.RegisterService(builder.Configuration);
+
 var app = builder.Build();
 
+app.MapControllers();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
